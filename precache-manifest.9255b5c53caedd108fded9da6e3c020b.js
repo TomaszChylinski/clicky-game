a10a8d1db124d97f696c23b2e920751b71f32d83
chylinski.t <chylinski.t@outlook.com>
2020-03-13 00:54:30 -0400

    Updates

---
+++ precache-manifest.9255b5c53caedd108fded9da6e3c020b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cabd81a58d83c26bfacd7b61420b6e3d",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "4390477e492c4edbb785",
+    "url": "/clicky-game/static/css/main.f06c1e63.chunk.css"
+  },
+  {
+    "revision": "6221061988481d078117",
+    "url": "/clicky-game/static/js/2.290a4c7d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/clicky-game/static/js/2.290a4c7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4390477e492c4edbb785",
+    "url": "/clicky-game/static/js/main.97afa480.chunk.js"
+  },
+  {
+    "revision": "84f183f776dec52d4ec4",
+    "url": "/clicky-game/static/js/runtime-main.5c9b0e5c.js"
+  }
+]);
